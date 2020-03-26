9f23c6e0e2dddf731d15cc3411c46f51ed921988
shadealao <alao.a.s@gmail.com>
2020-03-26 17:57:35 +0100

    resultat SOR entre gauss et methode des puissances

---
+++ test2.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+
+
+#define w 0.8
+
+typedef struct {
+	int i; //position i de l'élément
+	int j; //position j de l'élément
+	double p; //valeur élément
+}Element;
+
+typedef struct {
+	int m;  //nbr d'éléments non nuls de la matrice 
+	Element *T; //tableau d'elements
+	int n; //taille de la matrice carre
+	int *debCol;
+}Matrice;
+
+
+void init_element(Element *e, int i, int j, double p) {
+	e->i = i;
+	e->j = j;
+	e->p = p;
+}
+
+void init_matrice(Matrice *M, int m, int n) {
+	M->m = m;
+	M->T = malloc(m * sizeof(Element));
+	M->n = n;
+	M->debCol = malloc((n + 1) * sizeof(int));
+	int i ;
+	for(i = 0; i < n + 1; i++) {
+		M->debCol[i] = 0;
+	}
+}
+
+void free_matrice(Matrice *M) {
+	free(M->T);
+	free(M->debCol);
+}
+
+// Methode des puissances
+void puissance(Matrice M, double *x, int k, double *y){
+	int i,j;
+	for(i=0;i<M.n;i++){
+		for (j = M.debCol[i]; j < M.debCol[i + 1]; j++) {
+				y[i] += x[M.T[j].i] * M.T[j].p;
+			}
+	}
+}
+
+
+
+//combine y et z dans x
+void resultat (Matrice M, double *y, double *z, double *x) {
+	int i; 
+	for (i = 0; i < M.n; i++) {
+		x[i] = (w * y[i]) + ((1 - w) * z[i]); 
+	}
+}
+
+
+int main(){
+	
+	
+	
+	
+	
+	return 0;
+}
+	
